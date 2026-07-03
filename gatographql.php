020f9c493279195c4a7c44c70ca4ac947b60533b
leoloso <leo@getpop.org>
2026-07-03 09:41:13 +0000

    Merge pull request #3347 from GatoGraphQL/Distribute-Gato-GraphQL-via-FluentCart-not-WP-org
    Distribute Gato GraphQL plugin via the Gato Plugins store (instead of wordpress.org)

--- gatographql.php
+++ gatographql.php
@@ -4,7 +4,7 @@ Plugin Name: Gato GraphQL
 Plugin URI: https://gatographql.com
 GitHub Plugin URI: https://github.com/GatoGraphQL/GatoGraphQL
 Description: Powerful and flexible GraphQL server for WordPress.
-Version: 19.0.0
+Version: 19.1.0-dev
 Requires at least: 6.1
 Requires PHP: 8.1
 Author: Gato GraphQL
@@ -16,6 +16,8 @@ Domain Path: /languages
 GitHub Plugin URI: GatoGraphQL/gatographql-dist
 */
 
+use GatoGraphQL\GatoGraphQL\Constants\ExtensionDataOptions;
+use GatoGraphQL\GatoGraphQL\Marketplace\Constants\MarketplaceVersion;
 use GatoGraphQL\GatoGraphQL\Plugin;
 use GatoGraphQL\GatoGraphQL\PluginApp;
 use PoPIncludes\GatoGraphQL\Startup;
@@ -40,8 +42,9 @@ if (!defined('ABSPATH')) {
  *
  * @gatographql-readonly-code
  */
-$pluginVersion = '19.0.0';
+$pluginVersion = '19.1.0-dev';
 $pluginName = 'Gato GraphQL';
+$pluginProductName = 'Gato GraphQL';
 
 /**
  * If the plugin is already registered, halt loading
@@ -109,3 +112,22 @@ PluginApp::getMainPluginManager()->register(new Plugin(
     $pluginName,
     $commitHash
 ))->setup();
+
+// Validate the license
+$extensionManager = PluginApp::getExtensionManager();
+if (!$extensionManager->assertCommercialLicenseHasBeenActivated(
+    __FILE__,
+    $pluginProductName,
+    $pluginName,
+    $pluginVersion,
+    [
+        ExtensionDataOptions::CHANGELOG_URL => 'https://gatographql.com/changelog',
+        ExtensionDataOptions::HOMEPAGE_URL => 'https://gatographql.com',
+        ExtensionDataOptions::MARKETPLACE_PRODUCT_IDS => [
+            MarketplaceVersion::V2_FLUENTCART => 249,
+        ],
+        ExtensionDataOptions::IS_LICENSE_NEEDED => false,
+    ]
+)) {
+    return;
+}
